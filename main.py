0b3fa39f4b7775af2910dfbd1b5488133e46ffe3
Scott Johnson <kyle1940@gmail.com>
2021-02-15 23:05:22 -0800

    added get_char_generator_stats() to get an idea of how often 4's happen

--- main.py
+++ main.py
@@ -34,12 +34,42 @@ def generate_character_pair(target_points: int, max_correlation_coeff: float):
         if r <= max_correlation_coeff:
             return char1, char2, r
 
+def get_char_generator_stats():
+    n_char1_has_four = 0
+    n_char2_has_four = 0
+    n_both_has_four = 0
+    n = 10000
+    for i in range(n):
+        char1, char2, r = generate_character_pair(10, -0.1)
+        char1_has_four = False
+        char2_has_four = False
+        for stat in char1:
+            if char1[stat] == 4:
+                char1_has_four = True
+                break
+        for stat in char2:
+            if char2[stat] == 4:
+                char2_has_four = True
+                break
+        if char1_has_four:
+            n_char1_has_four += 1
+        if char2_has_four:
+            n_char2_has_four += 1
+        if char1_has_four and char2_has_four:
+            n_both_has_four += 1
+    p_char1_has_four = n_char1_has_four / n
+    p_char2_has_four = n_char2_has_four / n
+    print('Proportion Char 1 has a four:', p_char1_has_four)
+    print('Proportion Char 2 has a four:', p_char2_has_four)
+    print('Proportion both have a four', n_both_has_four / n, 'expected', p_char1_has_four * p_char2_has_four)
+
 
 def main():
-    char1, char2, r = generate_character_pair(10, -0.10)
-    print(char1)
-    print(char2)
-    print(r)
+    # char1, char2, r = generate_character_pair(10, -0.10)
+    # print(char1)
+    # print(char2)
+    # print(r)
+    get_char_generator_stats()
 
 
 if __name__ == '__main__':
